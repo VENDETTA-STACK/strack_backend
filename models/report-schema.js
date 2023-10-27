1e636542148f1034109b6356729a2850e290d38e
monil <user@Monil-MacBook.local>
2023-10-27 13:08:56 -0400

    db setup on atlas

--- models/report-schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const reportSchema = mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId
-    }
-});
-
-module.exports = mongoose.model('reports', reportSchema);
--- routes/api/inputs.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const inputController = require('../../controllers/inputs');
-const authController = require('../../middleware/auth');
-
-router.post('/', authController, inputController.addExpenseCategory);
-router.get('/', authController, inputController.getExpenseCategory);
-
-module.exports = router;
